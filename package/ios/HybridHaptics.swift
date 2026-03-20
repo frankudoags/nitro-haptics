93ab31718b77eea70ec8fbd4423779b0057a8bc8
frankudoags <frankudoags.com>
2026-03-20 23:31:01 +0100

    feat: implement haptic feedback triggers for various styles in HybridHaptics

--- package/ios/HybridHaptics.swift
+++ package/ios/HybridHaptics.swift
@@ -3,7 +3,36 @@ import NitroModules
 
 class HybridHaptics: HybridHapticsSpec {
     func trigger(style: HapticStyle) throws {
-        <#code#>
+        switch style {
+        case .selection:
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        case .impactLight:
+            let generator = UIImpactFeedbackGenerator(style: .light)
+            generator.prepare()
+            generator.impactOccurred()
+        case .impactMedium:
+            let generator = UIImpactFeedbackGenerator(style: .medium)
+            generator.prepare()
+            generator.impactOccurred()
+        case .impactHeavy:
+            let generator = UIImpactFeedbackGenerator(style: .heavy)
+            generator.prepare()
+            generator.impactOccurred()  
+        case .notificationSuccess:
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(.success)
+        case .notificationWarning:
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(.warning)
+        case .notificationError:
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(.error)
+        }
     }
     
 }
